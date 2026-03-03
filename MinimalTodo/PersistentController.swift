f89280e0582599f2921bb58845113cf5f866f875
John Dutamby <johnrenko@users.noreply.github.com>
2026-03-03 18:07:21 +0100

    Add README with project overview and usage instructions

--- MinimalTodo/PersistentController.swift
+++ MinimalTodo/PersistentController.swift
@@ -7,26 +7,31 @@ struct PersistenceController {
 
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "TodoListModel")
+
         if inMemory {
             container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
         }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+
+        container.persistentStoreDescriptions.forEach {
+            $0.setOption(true as NSNumber, forKey: NSMigratePersistentStoresAutomaticallyOption)
+            $0.setOption(true as NSNumber, forKey: NSInferMappingModelAutomaticallyOption)
+        }
+
+        container.loadPersistentStores { _, error in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
-        })
+        }
     }
-    
+
     func save() {
         let context = container.viewContext
         if context.hasChanges {
             do {
                 try context.save()
             } catch {
-                // replace this with code to handle the error appropriately
                 fatalError("Unresolved error \(error)")
             }
         }
     }
 }
-
